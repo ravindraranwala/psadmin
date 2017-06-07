9cb7db4b04c6746b68db8823a88fbc41fa35bd91
Ranwala, Ravindra <Ravindra.Ranwala@pearson.com>
2017-06-07 08:55:33 +0530

    bare bone routing was added.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,33 @@
 $ = jQuery = require('jquery');
+var React = require('react');
+var Home = require('./components/homePage');
+var About = require('./components/about/aboutPage');
 
-var App = console.log('Hello World from Browserify');
+(function(win) {
+  "use strict";
+  var App = React.createClass({
+    render: function() {
+      var Child;
 
-module.exports = App;
+      switch (this.props.route) {
+        case 'about': Child = About; break;
+        default: Child = Home;
+      }
+
+      return (
+        <div>
+          <Child />
+        </div>
+      );
+    }
+  });
+
+  function render() {
+    var route = win.location.hash.substr(1);
+    console.log(route);
+    React.render(<App route={route} />, document.getElementById('app'));
+  }
+
+  win.addEventListener('hashchanged', render);
+  render();
+})(window);
